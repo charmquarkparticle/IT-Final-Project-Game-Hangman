2d54c0f9e7eb1865ce6ce0a74925bdd6f953ffbc
charmquarkparticle <150382283+charmquarkparticle@users.noreply.github.com>
2024-03-01 19:56:19 +0400

    Add files via upload

---
+++ IT.Final.Project.Game.Hangman/IT.Final.Project.Game.Hangman/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+
+class Hangman
+{
+    static void Main(string[] args)
+    {
+        string[] words = { "electricity", "garbage", "grass", "matter", "newspaper", "orange", "pencil", "Appearance", "bathroom", "department", "homework", "language", "midnight", "obligation" };
+        string guessTheWord = words[new Random().Next(words.Length)];   
+
+        char[] guessTheLetters = new char[guessTheWord.Length];
+        int chance = 12;
+
+        Console.WriteLine("guess the word:");
+
+        for (int i = 0; i < guessTheWord.Length; i++)
+        {
+            guessTheLetters[i] = '*';                   
+            Console.Write("* ");
+        }
+        Console.WriteLine();
+
+        while (chance > 0)
+        {
+            Console.WriteLine($"You have {chance} attempts left");
+            Console.Write("Guess the letter: ");
+            char enteredLetter = char.ToLower(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+
+            if (!char.IsLetter(enteredLetter))   
+            {
+                Console.WriteLine("Enter only one letter ! ");
+                continue;
+            }
+
+            if (guessTheWord.Contains(enteredLetter))    
+            {
+                for (int i = 0; i < guessTheWord.Length; i++)
+                {
+                    if (guessTheWord[i] == enteredLetter)
+                    {
+                        guessTheLetters[i] = enteredLetter;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("You could not guess the correct letter");
+                chance--;
+            }
+
+            Console.WriteLine(string.Join(" ", guessTheLetters));
+
+            if (!guessTheLetters.Contains('*'))   
+            {
+                Console.WriteLine("You guessed the word correctly");
+                return;
+            }
+        }
+
+        Console.WriteLine("Unfortunately, you could not guess the word, the chances of recognizing the letters are over. The guess word was: ყ" + guessTheWord);
+    }
+}
